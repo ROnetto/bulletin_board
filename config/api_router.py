6165eb4cd3bc75335626e2e1f2134ab0da347404
Rodrigo <rodrigo.onetto.cortes@gmail.com>
2021-10-08 18:28:53 -0300

    Removing urls

--- config/api_router.py
+++ config/api_router.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from bulletin_board.apartments.api.views import ApartmentViewSet
+from bulletin_board.building_floors.api.views import BuildingFloorViewSet
+from bulletin_board.buildings.api.views import BuildingViewSet
+from bulletin_board.bulletin_boards.api.views import (
+    BuildingBulletinBoardViewSet,
+    CommunityBulletinBoardViewSet,
+)
 from bulletin_board.communities.api.views import CommunityViewSet
 from bulletin_board.users.api.views import UserViewSet
 
@@ -9,6 +16,11 @@ if settings.DEBUG:
 else:
     router = SimpleRouter()
 
+router.register("apartments", ApartmentViewSet)
+router.register("building_floors", BuildingFloorViewSet)
+router.register("buildings", BuildingViewSet)
+router.register("building_bulletin_boards", BuildingBulletinBoardViewSet)
+router.register("community_bulletin_boards", CommunityBulletinBoardViewSet)
 router.register("communities", CommunityViewSet)
 router.register("users", UserViewSet)
 
